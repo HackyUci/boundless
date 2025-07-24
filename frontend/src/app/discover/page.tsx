32983219f221e0bcc4889ca5698c57d6e88187ef
widaputri <widapk2709@gmail.com>
2025-07-25 03:28:37 +0700

    feat: move discover page to discover endpoint

---
+++ frontend/src/app/discover/page.tsx
@@ -0,0 +1,12 @@
+import ResizableLayout from "@/components/resizeable-layout/ResizeableLayout"
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ResizableLayout />
+    </div>
+  )
+}
+
+export default page
