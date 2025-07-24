1c2e90ca5350c983c1bb4c25a3b175fc7fbdbc85
gracekarinn <gracekarina12@gmail.com>
2025-07-24 22:45:31 +0700

    feat: create module

---
+++ frontend/src/modules/CVScannerModule/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AnalyzeResultSection } from './sections/AnalyzeResultSection'
+
+export const CVScannerModule = () => {
+  return (
+    <div>
+      <AnalyzeResultSection />
+    </div>
+  )
+}
+
+
