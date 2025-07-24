514cfb731f2ac130f03fbffae26e437801a8b053
gracekarinn <gracekarina12@gmail.com>
2025-07-25 00:59:38 +0700

    feat: add ai module with chatbot section component

---
+++ frontend/src/modules/AIChatbotModule/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ChatbotSection } from './sections/ChatbotSection'
+
+export const AIChatbotModule = () => {
+  return (
+    <div>
+      <ChatbotSection />
+    </div>
+  )
+}
+
