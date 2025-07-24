d724dbad7d7c689281b3f4567d6123e87fcf0939
gracekarinn <gracekarina12@gmail.com>
2025-07-24 22:44:15 +0700

    feat: add result page component with cv scanner module

---
+++ frontend/src/app/result/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CVScannerModule } from '@/modules/CVScannerModule'
+
+const page = () => {
+  return (
+    <div>
+      <CVScannerModule />
+    </div>
+  )
+}
+
+export default page
+
+  
